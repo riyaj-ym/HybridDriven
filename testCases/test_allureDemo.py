fa777fecb491aa536c1bf9bb22d131b9db2298d5
riyaj-ym <riyaj.ym@gmail.com>
2022-10-02 18:23:23 +0530

    allure demo test

--- testCases/test_allureDemo.py
+++ testCases/test_allureDemo.py
@@ -1,14 +1,51 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
 import pytest
 import allure
-from webdriver_manager.chrome import ChromeDriverManager
+from allure_commons.types import AttachmentType
+from selenium.webdriver.common.by import By
 
 
+@allure.severity(allure.severity_level.NORMAL)
 class TestAllure:
-    def test_Logo(self, welcome):
-        ser_obj = Service(ChromeDriverManager().install())
-        self.driver = webdriver.Chrome(service=ser_obj)
-        self.driver.maximize_window()
-        self.driver.get("https://www.facebook.com/")
-        self.driver.close()
+    @allure.severity(allure.severity_level.MINOR)
+    def test_Logo(self, setup, welcome):
+        driver = setup
+        driver.maximize_window()
+        driver.get("https://www.facebook.com/")
+        status = driver.find_element(By.XPATH, "//img[@alt='Facebook']").is_displayed()
+        if status:
+            assert True
+        else:
+            assert False
+        driver.close()
+
+    @allure.severity(allure.severity_level.NORMAL)
+    @pytest.mark.skip("Skipping this, after fixing issue will run")
+    def test_listEmployees(self, setup, welcome):
+        pass
+
+    @allure.severity(allure.severity_level.CRITICAL)
+    def test_loginNopCom(self, setup, welcome):
+        driver = setup
+        driver.maximize_window()
+        driver.get("https://admin-demo.nopcommerce.com/login?ReturnUrl=%2Fadmin%2F")
+        driver.find_element(By.XPATH, "//input[@id='Email']").clear()
+        driver.find_element(By.XPATH, "//input[@id='Email']").send_keys("admin@yourstore.com")
+
+        driver.find_element(By.XPATH, "//input[@id='Password']").clear()
+        driver.find_element(By.XPATH, "//input[@id='Password']").send_keys("admin")
+
+        driver.find_element(By.XPATH, "//button[@type='submit']").click()
+
+        exp_Title = "Dashboard / nopCommerce administration1"
+        act_Title = driver.title
+
+        if exp_Title == act_Title:
+            assert True
+            driver.close()
+        else:
+            allure.attach(driver.get_screenshot_as_png(), name="testLoginScreen", attachment_type=AttachmentType.PNG)
+            driver.close()
+            assert False
+
+
+
